2ceec258763f07d456ba7d80d9bfa3bc370a941f
sagar <sagarrpatil9607@gmail.com>
2024-04-11 11:50:20 +0530

    setup

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,9 @@
 import { useRoutes } from 'react-router-dom';
-
-// routes
 import MainRoutes from './MainRoutes';
 import AuthenticationRoutes from './AuthenticationRoutes';
-
-// ==============================|| ROUTING RENDER ||============================== //
-
 export default function ThemeRoutes() {
-  if(localStorage.getItem("token")){
-    return useRoutes([MainRoutes]);
-  }else{
-    return useRoutes([AuthenticationRoutes]);
-  }
+  const token = localStorage.getItem("token");
+  const routes = token ? [MainRoutes] : [AuthenticationRoutes];
+  return useRoutes(routes);
 }
+
