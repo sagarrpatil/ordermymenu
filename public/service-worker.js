4c7081518a2d6d4a643a2b6146f19aeafb928fc9
sagar <sagarrpatil9607@gmail.com>
2024-06-15 22:58:03 +0530

    set dqata

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "0.0.8";
+const CACHE_NAME = "0.0.9";
 const STATIC_CACHE_URLS = ["/", "/index.html", "/manifest.json", /* Add other static asset URLs */];
 
 this.self.addEventListener("install", (event) => {
